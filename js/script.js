1e31f201798b0be286129a21b82fb2676d3fddb2
Yuri629 <manteng629@gmail.com>
2023-12-25 00:43:51 +0800

    Updated 25/12

--- js/script.js
+++ js/script.js
@@ -22,38 +22,3 @@ Inputs.forEach((input) => {
     input.addEventListener("focus",focusFunc);
     input.addEventListener("blur",blurFunc);
 })
-
-/*====================Send message to the email===============*/
-const contactForm = document.getElementById('contact_form'),
-      contactMessage = document.getElementById('contact_message')
-
-//declare the sendEmail function
-const sendEmail = (e) => {
-    //prevent the default behavior of the form
-    e.preventDefault()
-
-    //send the form data using emailjs
-    //serviceID - templateID - #form - publicKey(it is in the EmailJS side bar = account)
-    emailjs.sendForm('service_4dvqzzg','template_ozuycat','#contact_form','IAq16adwb63V6UeNa')
-        .then(() =>{
-            //Show sent message
-            contactMessage.textContent = 'Message sent successfully ✅'
-
-            //Remove the message after five seconds
-            setTimeout(() => 
-            {
-                //set time with function and milisecond
-                //how many second need to add on x000, such as 3000 = 3 second
-                contactMessage.textContent = ''}, 5000)
-
-                //Clear input file
-                contactForm.reset()
-
-            },() => {
-                //show error message
-                contactMessage.textContent = 'Message sent unsucessfully ❌'
-            })
-}
-
-//send the conatctForm to my email
-contactForm.addEventListener('submit',sendEmail)
