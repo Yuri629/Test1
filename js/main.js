6615cb920be3be4d2e39bb8db15a6f6b38de2547
Yuri629 <manteng629@gmail.com>
2024-01-09 21:14:35 +0800

    Updated 9/1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 //toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
 }
 
 //scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 //when the window start to scroll
 window.onscroll = () => {
@@ -22,74 +22,74 @@ window.onscroll = () => {
         //height = the height of the section
         let height = sec.offsetHeight;
         //id = the id of the each section
-        let id = sec.getAttribute('id');
+        let id = sec.getAttribute("id");
 
         if(top >= offset && top < offset + height){
             //active navbar Links
             //the links in the () is used to declare and use in => {}
-            navLinks.forEach(links =>{
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            navLinks.forEach(links => {
+                links.classList.remove("active");
+                document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
             });
         }
     })
     //sticky header
-    let header = document.querySelector('header');
+    let header = document.querySelector("header");
 
-    header.classList.toggle('sticky',window.scrollY > 100);
+    header.classList.toggle("sticky",window.scrollY > 100);
 
     //remove toddle icon and navbar whne click navbar links(scroll) 
-    menuIcon.classList.remove('bx_x');
-    navbar.classList.remove('active');
+    menuIcon.classList.remove("bx_x");
+    navbar.classList.remove("active");
 }
 
 /*=========================Show Scroll Up===================*/
 const scrollUp = () =>{
-    const scrollUp = document.getElementById('scroll-up')
+    const scrollUp = document.getElementById("scroll-up")
     //when the scroll is higher than 350 viewport height, add show-scroll class to the tag with the scroll up class
     //it is the shorthand way to write [if...else]
     //this.scroolY is check if the current scroll position is > 350px
-    //it execute the first expression after ? which is add 'show-scroll'(already declare in css)
-    //':' is the the second expression when the statement is false
-    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-                          :scrollUp.classList.remove('show-scroll') 
+    //it execute the first expression after ? which is add "show-scroll"(already declare in css)
+    //":" is the the second expression when the statement is false
+    this.scrollY >= 350 ? scrollUp.classList.add("show-scroll")
+                          :scrollUp.classList.remove("show-scroll") 
 }
 
-window.addEventListener('scroll', scrollUp)
+window.addEventListener("scroll", scrollUp)
 
 /*=========================DARK LIGHT THEME==================*/
-const themeButton = document.getElementById('theme-button')
+const themeButton = document.getElementById("theme-button")
 //in CSS, it is body.dark-theme
-const darktheme = 'dark-theme'
+const darktheme = "dark-theme"
 //it already link to the boxicon, so it just need the name of the class
 //in box icon to change
-const icontheme = 'bx-sun'
+const icontheme = "bx-sun"
 
 //We obtain the current theme that the onterface has by validating the dark theme class
 //document.body is use to declare the body in the CSS
-const getCurrentTheme = () => document.body.classList.contains(darktheme) ? 'dark' : 'light'
+const getCurrentTheme = () => document.body.classList.contains(darktheme) ? "dark" : "light"
 //ThemeButton was declare in the CSS line 53
-const getCurrentIcon = () => themeButton.classList.contains(icontheme) ? 'bx bx-moon' : 'bx bx-sun'
+const getCurrentIcon = () => themeButton.classList.contains(icontheme) ? "bx bx-moon" : "bx bx-sun"
 
 //Previously selected topic (if user selected)
-//LocalStorage = use to store the data on user's browser
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+//LocalStorage = use to store the data on user"s browser
+const selectedTheme = localStorage.getItem("selected-theme")
+const selectedIcon = localStorage.getItem("selected-icon")
 
 //We validate if the user previously chose a topic
 if(selectedTheme){
     //if the validation is fulfilled we ask what the issue was to know if we activated or deal
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darktheme)
-    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](icontheme)
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darktheme)
+    themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](icontheme)
 }
 
 //Activate / deactivate the theme manually with the button 
-themeButton.addEventListener('click', () => {
+themeButton.addEventListener("click", () => {
     //Add or remove the dark / icon theme
     document.body.classList.toggle(darktheme)
     themeButton.classList.toggle(icontheme)
     //We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme',getCurrentTheme())
-    localStorage.setItem('selectedIcon',getCurrentIcon())
+    localStorage.setItem("selected-theme",getCurrentTheme())
+    localStorage.setItem("selectedIcon",getCurrentIcon())
 })
 
